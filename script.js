69cb84f0bfd0338b45b5112f7226afcf904db89f
vincent <nardozzi24@gmail.com>
2018-04-14 13:28:40 -0400

    basic ajax to work with

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let paramObject = { 'api-key': "7f930f9b1b5c41caa08a3d472445a87c"}
+
+makeTheCall = function() {
+var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+url += '?' + $.param(paramObject);
+$.ajax({
+  url: url,
+  method: 'GET',
+}).then(function(result) {
+
+});
+}
+
+document.on("click", function() {
+
+
+})
+
+
+
+
+
